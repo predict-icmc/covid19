3c226120805566553b466b92f25ea7c735ec1214
Flaviane Louzeiro <flalouzeiro@hotmail.com>
2020-05-21 22:19:53 -0300

    protótipos de plotagem

---
+++ plotsMaps/mapasBrasil.R
@@ -0,0 +1,54 @@
+##### 
+##### https://dataficacao.wordpress.com/2017/02/21/criando-mapa-brasil-r/
+#####
+
+library(rgdal)
+library(RColorBrewer)
+library(leaflet)
+setwd("~/Documentos/USP/predict/covid19/plotsMaps/")
+
+mapa <- readOGR("2015/br_unidades_da_federacao/BRUFE250GC_SIR.shp", stringsAsFactors=FALSE, encoding="UTF-8")
+mapa@data
+pg <- read.csv("densidadeDemografica.csv", sep = ",")
+pg <- as.data.frame(pg)
+head(pg)
+brasileiroHab <- merge(mapa,pg, by.x = "CD_GEOCUF", by.y = "COD_UF")
+brasileiroHab@data
+
+proj4string(brasileiroHab) <- CRS("+proj=longlat +datum=WGS84 +no_defs")
+Encoding(brasileiroHab$NM_ESTADO) <- "UTF-8"
+brasileiroHab$hab_km_quad[is.na(brasileiroHab$hab_km_quad)] <- 0
+
+pal <- colorBin("Greens",domain = NULL,n=5) 
+
+state_popup <- paste0("<strong>Estado: </strong>", 
+                      brasileiroHab$NM_ESTADO, 
+                      "<br><strong>Habitantes por Km Quadrado: </strong>", 
+                      brasileiroHab$hab_km_quad)
+leaflet(data = brasileiroHab) %>%
+  addProviderTiles("CartoDB.Positron") %>%
+  addPolygons(fillColor = ~pal(brasileiroHab$hab_km_quad), 
+              fillOpacity = 0.8, 
+              color = "#BDBDC3", 
+              weight = 1, 
+              popup = state_popup) %>%
+  addLegend("bottomright", pal = pal, values = ~brasileiroHab$hab_km_quad,
+            title = "Densidade Demográfica",
+            opacity = 1)
+
+#################### segundo tutorial
+## http://rstudio-pubs-static.s3.amazonaws.com/334254_db4a393ba750464fa36349f2a2991b6a.html
+#####
+
+library(sp)
+n.cat = 8
+
+#Cores utilizadas para cada categoria
+cores = c("#03E06EFF", "#38FA2AFF", "#9FFF15FF", "#DEFE0BFF", "#F3FB06FF", "#FFCD00FF", "#FF8500FF", "#FF3300FF")
+intervalos = quantile(brasileiroHab$hab_km_quad, probs = seq(0,1,0.125)) + c(-0.001,rep(0,7),0.001)
+spplot(brasileiroHab,c("hab_km_quad"),
+       at=intervalos,
+       ylab = "Densidade demográfica",
+       col.regions =brewer.pal(8, "Greens")) #Outras opções de cores: Greens, BrBG, Accent
+
+
