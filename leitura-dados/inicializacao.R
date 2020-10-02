6d45c9cd19399699ff0f37e9080f9c068583f1b1
abner1350 <abner.luz@usp.br>
2020-10-02 14:29:01 -0300

    Rotina Dinaria

--- leitura-dados/inicializacao.R
+++ leitura-dados/inicializacao.R
@@ -1,6 +1,19 @@
-######Inicialização####################
-#1- Rode os
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-source("merge-data.R")
-
-pegaCorona()
+####################Inicialização############################
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) #
+if(!require(tidyverse)) install.packages("tidyverse")       #
+if(!require(feather)) install.packages("feather")           #
+if(!require(data.table)) install.packages("data.table")     #
+source("merge-data.R")                                      #
+pegaCorona()                                                #
+#############################################################
+latlong <- "latlong-covid.feather"                          #
+dadoslat <- read_feather(latlong)                           #
+#############################################################
+caso_full <- "full-covid.feather"                           #
+df<-read_feather(caso_full)                                 #
+#############################################################
+cartorio <- "ob-cartorio.feather"                           #
+df.cartorio <- read_feather(cartorio)                       #
+#############################################################
+demografico <- read.csv2("Dados_demograficos.csv",sep=",")  #
+#############################################################
