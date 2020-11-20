66dbe4a12065aa70806cf8163431d2d505be4a64
Valete-y <65100537+Valete-y@users.noreply.github.com>
2020-11-20 00:10:17 -0300

    adsadadsada

--- gráficos corr.R
+++
@@ -1,6 +0,0 @@
-#bancos de dados a serem utilizados + inicialização 
-
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-source("merge-data.R")
-pegaCorona()
-demografico <- read.csv2("Dados_demograficos.csv",sep=",")
