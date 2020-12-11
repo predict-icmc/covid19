72150f7647058b67688048491918ab26d0b7ab4d
abner1350 <abner.luz@usp.br>
2020-12-10 23:57:05 -0300

    redes neurais

--- Abner/Redes neurais.R
+++ Abner/Redes neurais.R
@@ -1,31 +1,40 @@
-install.packages('forecast', 'fpp')
+#install.packages('forecast', 'fpp')
 library(forecast)
 library(fpp)
 library(tidyverse)
 library(scales)
-View(df)
 
+#Separando o banco de dados para apenas o estado de são paulo
 redes <-df%>%
   filter(state=="SP" & place_type=="state") %>% 
   select(new_confirmed,date)
 
+#Não precisa padronizar os dados eu acho(?)
 #redes$new_confirmed<-rescale(redes$new_confirmed,to=c(0,1))
+
+#Dados visualização
 summary(redes)
 View(redes)
+
+#Transformar os dados em series temporais
+#Não consegui colocar na função de uma maneira facil
 #redes<- ts(redes)
 
+#Essa função usar para tentar achar o melhor lambda porem não tenho certeza 
+#Se é essas função que deve ser usada
 ?BoxCox.lambda
 BoxCox.lambda(redes$new_confirmed)
-fit <- nnetar(redes$new_confirmed,p=24,subset =redes$new_confirmed[1:270])
-?nnetar
+
+#Fitar o modelo com 270 linhas dos casos confirmados
+fit <- nnetar(redes$new_confirmed,lambda=0.07,subset =redes$new_confirmed[1:270])
 summary(fit)
+
+#Plot dos dados previsto
+#não como colocar os dados test e o verdadeiros com cores diferentes
 autoplot(forecast(fit))
-?forecast
-shapiro.test(redes$new_confirmed)
-?nnetar
+
+
+#Teste de normalidade 
+#shapiro.test(redes$new_confirmed)
 ##################################################
-?ts
-
-
-?forecast.nnetar
 
