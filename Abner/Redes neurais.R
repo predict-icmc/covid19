3f022b42d421d6424b8b26c73764592535eb5335
abner1350 <abner.luz@usp.br>
2020-12-11 14:46:48 -0300

    redes neurais atualização

--- Abner/Redes neurais.R
+++ Abner/Redes neurais.R
@@ -3,6 +3,8 @@ library(forecast)
 library(fpp)
 library(tidyverse)
 library(scales)
+library(modelr)
+library(plotly)
 
 #Separando o banco de dados para apenas o estado de são paulo
 redes <-df%>%
@@ -26,15 +28,38 @@ View(redes)
 BoxCox.lambda(redes$new_confirmed)
 
 #Fitar o modelo com 270 linhas dos casos confirmados
-fit <- nnetar(redes$new_confirmed,lambda=0.07,subset =redes$new_confirmed[1:270])
+treino<-redes$new_confirmed[1:276]
+test<-redes$new_confirmed[277:290]
+fit <- nnetar(treino,lambda ="auto",p=7)
+
 summary(fit)
 
 #Plot dos dados previsto
 #não como colocar os dados test e o verdadeiros com cores diferentes
-autoplot(forecast(fit))
 
+autoplot(forecast(fit,h=15))
 
 #Teste de normalidade 
 #shapiro.test(redes$new_confirmed)
 ##################################################
 
+#Prevendo os dados 
+predict(fit,14)
+v<-data.frame(forecast(fit,14))
+
+#EQM
+sqrt(sum((v$Point.Forecast-test)^2))
+
+#plot dados treinos
+
+grafico<-data.frame(predito=v,test=test)
+
+treino<-data.frame(treino=treino)
+p<-ggplot()+
+  geom_line(aes(x=seq(1:276),y=treino$treino),col="Blue")+
+  geom_line(aes(x=seq(277,290),y=grafico$Point.Forecast),col="Red")+
+  geom_line(aes(x=seq(277,290),y=grafico$test),col="Orange")+
+  scale_color_discrete(name = "Y series", labels = c("Y3","Y2", "Y1"))
+
+ggplotly(p)  
+  
