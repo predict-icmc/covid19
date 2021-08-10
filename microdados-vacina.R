1862c4d8020eeae9c332ad46433d855df7662ebf
chicodias <chicodias@outlook.com>
2021-08-10 15:10:59 -0300

    microdados vacina

--- microdados-vacina.R
+++ microdados-vacina.R
@@ -1,6 +1,6 @@
 #' Script que recebe um arquivo .csv dos dados de vacinacao DATASUS
 #' e computa o numero de vacinados por faixa etaria e sexo
-#' 
+#'
 #' mais informações e download dos datasets em https://opendatasus.saude.gov.br/dataset/covid-19-vacinacao
 
 library(disk.frame)
@@ -21,36 +21,16 @@ some.df <- csv_to_disk.frame("vc.csv", outdir = "some.df", in_chunk_size = 1e6) 
 
 # computa a frequencia de cada um dos grupos e converte no formato usual do R
 vacinados <- some.df %>%
-  group_by(paciente_enumsexobiologico, paciente_idade)  %>%
+  group_by(paciente_enumSexoBiologico, paciente_idade)  %>%
   summarise(n = n()) %>%
   collect()
 
 # computa a frequencia de vacinados em cada municipio e converte no formato usual do R
 vacinados_municip <- some.df %>%
-  group_by(estabelecimento_municipio_nome, vacina_dataaplicacao, vacina_descricao_dose)  %>%
+  group_by(paciente_endereco_coIbgeMunicipio, vacina_dataAplicacao, vacina_nome)  %>%
   summarise(n = n()) %>%
   collect()
 
-
-# computa a frequencia de vacinados em cada municipio e converte no formato usual do R
-vacinados_por_grupo <- some.df %>%
-  group_by(vacina_dataaplicacao, vacina_grupoatendimento_nome, vacina_descricao_dose)  %>%
-  summarise(n = n()) %>%
-  collect()
-
-# computa a frequencia de vacinados em cada municipio e converte no formato usual do R
-vacinados_sp <- some.df %>%
-  group_by(vacina_dataaplicacao, vacina_nome, vacina_descricao_dose)  %>%
-  summarise(n = n()) %>%
-  collect()
-
-
-colnames(vacinados_municip) <- c("municip","data","dose","n")
-colnames(vacinados_por_grupo) <- c("data","grupo","dose","n")
-colnames(vacinados_sp) <- c("data","vacina","dose","n")
 
 # para salvar
 write_csv(vacinados, "vacinados.csv")
-write_csv(vacinados_municip, "vacinados_munic.csv")
-write_csv(vacinados_por_grupo, "vacinados_grupo.csv")
-write_csv(vacinados_sp, "vacinados_sp.csv")
